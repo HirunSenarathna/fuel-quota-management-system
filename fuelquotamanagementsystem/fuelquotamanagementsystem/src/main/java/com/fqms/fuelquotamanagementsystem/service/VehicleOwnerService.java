1bf1f81f1c16afb292140edf8b771f5dbed4d49b
MishelLiyanage <mailtomishel@gmail.com>
2025-04-17 08:56:33 +0530

    implemented the vehicle owner registration

---
+++ fuelquotamanagementsystem/fuelquotamanagementsystem/src/main/java/com/fqms/fuelquotamanagementsystem/service/VehicleOwnerService.java
@@ -0,0 +1,8 @@
+package com.fqms.fuelquotamanagementsystem.service;
+
+import com.fqms.fuelquotamanagementsystem.Dtos.FuelStationRegistrationRequestDto;
+import com.fqms.fuelquotamanagementsystem.Dtos.VehicleOwnerRegistrationRequestDto;
+
+public interface VehicleOwnerService {
+    void registerVehicleOwner(VehicleOwnerRegistrationRequestDto request);
+}
