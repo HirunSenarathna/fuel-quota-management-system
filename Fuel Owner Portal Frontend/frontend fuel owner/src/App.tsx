2c8f55e116a772e5446be555d438e10f1a937b09
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-05-09 11:18:10 +0530

    add routing for SignIn and FuelOwnerDashboard pages in App component

--- Fuel Owner Portal Frontend/frontend fuel owner/src/App.tsx
+++ Fuel Owner Portal Frontend/frontend fuel owner/src/App.tsx
@@ -1,8 +1,19 @@
-import './App.css'
-import SignIn from './pages/SignIn'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import FuelOwnerDashboard from './pages/FuelOwnerDashboard';
 
 function App() {
-  return <SignIn />;
+  return (
+    <Router>
+      <Routes>
+        {/* Route for SignIn */}
+        <Route path="/" element={<SignIn />} />
+        {/* Route for FuelOwnerDashboard */}
+        <Route path="/dashboard" element={<FuelOwnerDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
