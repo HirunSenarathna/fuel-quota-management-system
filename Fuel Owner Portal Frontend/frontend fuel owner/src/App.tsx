1165cc46381dedfc8c9c777e06f484603406ee9f
Sanduni Pandipperuma <sandunipandipperuma@gmail.com>
2025-05-11 23:47:12 +0530

    Enhance vehicle registration form with dropdowns and add background styling

--- Fuel Owner Portal Frontend/frontend fuel owner/src/App.tsx
+++ Fuel Owner Portal Frontend/frontend fuel owner/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import FuelOwnerDashboard from './pages/FuelOwnerDashboard';
+import VehicleRegistration from './pages/VehicleRegistration';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<SignIn />} />
         {/* Route for FuelOwnerDashboard */}
         <Route path="/dashboard" element={<FuelOwnerDashboard />} />
+        <Route path="/vehicle-registration" element={<VehicleRegistration />} />
       </Routes>
     </Router>
   );
