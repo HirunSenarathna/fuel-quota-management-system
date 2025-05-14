78b986788ae3966096223eb8343d7d0eeb8c00fd
MishelLiyanage <mailtomishel@gmail.com>
2025-05-14 22:39:04 +0530

    resolved some warnings

--- fuelquotamanagementsystem/fuelquotamanagementsystem/src/main/java/com/fqms/fuelquotamanagementsystem/service/Impl/VehicleServiceImpl.java
+++ fuelquotamanagementsystem/fuelquotamanagementsystem/src/main/java/com/fqms/fuelquotamanagementsystem/service/Impl/VehicleServiceImpl.java
@@ -7,8 +7,6 @@ import com.fqms.fuelquotamanagementsystem.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
