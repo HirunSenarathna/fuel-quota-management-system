889161c98083d81b3a7e070b8d6caff9971dcb30
MishelLiyanage <mailtomishel@gmail.com>
2025-05-23 08:47:40 +0530

    Resolve warning

--- fuelquotamanagementsystem/fuelquotamanagementsystem/src/main/java/com/fqms/fuelquotamanagementsystem/repository/system/StationFuelQuantityRepository.java
+++ fuelquotamanagementsystem/fuelquotamanagementsystem/src/main/java/com/fqms/fuelquotamanagementsystem/repository/system/StationFuelQuantityRepository.java
@@ -2,7 +2,6 @@ package com.fqms.fuelquotamanagementsystem.repository.system;
 
 import com.fqms.fuelquotamanagementsystem.models.system.StationFuelQuantity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
